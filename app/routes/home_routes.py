43be9029efb86316c1574ccafd034c8caf6bfea3
evo <evolutionire@gmail.com>
2025-02-05 18:35:29 +0000

    First update

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -26,25 +26,9 @@ async def index(request: Request):
     # note passing of parameters to the page
     return templates.TemplateResponse("./home/index.html", {"request": request, "serverTime": serverTime })
 
-@router.get("/advice", response_class=HTMLResponse)
-async def advice(request: Request):
-    
-    # Define a request_client instance
-    requests_client = request.app.requests_client
 
-    # Connect to the API URL and await the response
-    response = await requests_client.get(config("ADVICE_URL"))
 
-    data = response.json()
 
-    # Send the json data from the response in the TemplateResponse data parameter 
-    return templates.TemplateResponse("./home/advice.html", {"request": request, "data": data })
-
-@router.get("/apod", response_class=HTMLResponse)
-async def apod(request: Request):
-    requests_client = request.app.requests_client
-    response = await requests_client.get(config("NASA_APOD_URL") + config("NASA_API_KEY"))
-    return templates.TemplateResponse("./home/apod.html", {"request": request, "data": response.json() })
 
 
 @router.post("/clicked", response_class=HTMLResponse)
@@ -61,7 +45,3 @@ async def index(request: Request):
     # send response
     return serverTime
 
-# https://www.getorchestra.io/guides/mastering-query-parameters-in-fastapi-a-detailed-tutorial
-@router.get("/params", response_class=HTMLResponse)
-async def params(request: Request, name : str | None = ""):
-    return templates.TemplateResponse("./home/params.html", {"request": request, "name": name })
